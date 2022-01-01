479b12ccec89f1e42fef302ab0578d69c194fd63
L-LYR <lisicen97@gmail.com>
2022-01-01 20:50:23 +0800

    feat: try to implement IsCopyConstructible and rename type_utils to type_traits

--- include/traits/type_traits.hpp
+++ include/traits/type_traits.hpp
@@ -2,10 +2,12 @@
 #define __CPP_UTILS_TRAITS_TYPE_UTILS__
 
 #include "traits/bool_utils.hpp"
+#include "traits/type_traits_impl.hpp"
 #include "traits/typed_constant.hpp"
 
 namespace cpp_utils {
 namespace traits {
+
 // helper
 
 template <typename T> struct RemoveReference { using type = T; };
@@ -21,6 +23,30 @@ template <typename T> struct IsRvalueReference<T &&> : TypedTrue {};
 template <typename T>
 struct IsReference : OR<IsLvalueReference<T>, IsRvalueReference<T>>::Result {};
 template <typename T> struct IsNotReference : NOT<IsReference<T>> {};
+
+template <typename T, typename = void>
+struct IsReferenceable : public TypedFalse {};
+template <typename T>
+struct IsReferenceable<T, VoidType<T &>> : public TypedTrue {};
+
+template <typename T, typename... Args>
+struct IsConstructible
+    : TypedBoolConstant<details::IsConstructibleImpl<T, Args...>()> {
+  // TODO: static_assert(is_complete_or_unbounded)
+};
+
+template <typename T, bool = IsReferenceable<T>::value>
+struct isCopyConstructibleImpl;
+template <typename T>
+struct isCopyConstructibleImpl<T, false> : public TypedFalse {};
+template <typename T>
+struct isCopyConstructibleImpl<T, true> : public IsConstructible<T, const T &> {
+};
+
+template <typename T>
+struct IsCopyConstructible : public isCopyConstructibleImpl<T> {
+  // TODO: static_assert(is_complete_or_unbounded)
+};
 
 // controller
 
