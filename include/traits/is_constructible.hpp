3452c03400f26c3020d380b1a61447dd6459c875
L-LYR <lisicen97@gmail.com>
2022-01-02 16:02:38 +0800

    feat: implement IsConstructible

--- include/traits/is_constructible.hpp
+++ include/traits/is_constructible.hpp
@@ -3,26 +3,66 @@
 
 #include "traits/bool_utils.hpp"
 #include "traits/declval.hpp"
+#include "traits/is_complete.hpp"
+#include "traits/is_destructible.hpp"
 #include "traits/result_type.hpp"
 
 namespace cpp_utils {
 namespace traits {
 namespace details {
 
+// regular case
 template <typename T, typename... Args,
           typename = decltype(T(declval<Args>()...))>
-auto IsConstructibleImplChecker(int) -> Ok;
-
-template <typename T, typename...> auto IsConstructibleImplChecker(...) -> Err;
+auto IsConstructibleRegularChecker(int) -> Ok;
+template <typename, typename...> auto IsConstructibleRegularChecker(...) -> Err;
+template <typename T, typename... Args>
+struct IsConstructibleRegularImpl
+    : public IsOk<decltype(IsConstructibleRegularChecker<T, Args...>(0))> {};
+// reference
+template <typename T> auto IsConstructibleRefChecker(T) -> Ok;
+template <typename T> auto IsConstructibleRefChecker(...) -> Err;
+template <typename T, typename U>
+struct IsConstructibleRefImpl : public TypedFalse {};
+template <typename LRef, typename U>
+struct IsConstructibleRefImpl<LRef &, U>
+    : public IsOk<decltype(IsConstructibleRefChecker<LRef &>(declval<U>()))> {};
+template <typename RRef, typename U>
+struct IsConstructibleRefImpl<RRef &&, U>
+    : public IsOk<decltype(IsConstructibleRefChecker<RRef &&>(declval<U>()))> {
+};
+// conversion
+template <typename T, typename U, typename = decltype(::new T(declval<U>()))>
+auto IsConstructibleConvChecker(int) -> Ok;
+template <typename, typename> auto IsConstructibleConvChecker(...) -> Err;
+template <typename T, typename U>
+struct IsConstructibleConvImpl
+    : public AND<IsDestructible<T>,
+                 IsOk<decltype(IsConstructibleConvChecker<T, U>(0))>> {};
 
 } // namespace details
 
 template <typename T, typename... Args>
 struct IsConstructible
-    : TypedBoolConstant<sizeof(details::IsConstructibleImplChecker<T, Args...>(
-                            0)) == sizeof(Ok)> {
-  // TODO: static_assert(is_complete_or_unbounded)
+    : public details::IsConstructibleRegularImpl<T, Args...> {
+  static_assert(IsComplete<T>::value, "Target type is imcomplete");
 };
+
+template <typename T, typename U>
+struct IsConstructible<T, U> : public details::IsConstructibleConvImpl<T, U> {};
+
+template <typename LRef, typename U>
+struct IsConstructible<LRef &, U>
+    : public details::IsConstructibleRefImpl<LRef &, U> {};
+
+template <typename RRef, typename U>
+struct IsConstructible<RRef &&, U>
+    : public details::IsConstructibleRefImpl<RRef &&, U> {};
+
+template <> struct IsConstructible<void> : public TypedFalse {};
+template <> struct IsConstructible<void const> : public TypedFalse {};
+template <> struct IsConstructible<void volatile> : public TypedFalse {};
+template <> struct IsConstructible<void const volatile> : public TypedFalse {};
 
 template <typename T, typename... Args>
 struct IsNotConstructible : NOT<IsConstructible<T, Args...>> {};
--- tests/is_constructible_test.cpp
+++ tests/is_constructible_test.cpp
@@ -1,29 +1,44 @@
 #include "traits/is_constructible.hpp"
 #include "gtest/gtest.h"
+#include <type_traits>
+
+struct UndefStruct;
 
 struct TestStruct {
   int i;
   float f;
 
   TestStruct(int i, float f) : i(i), f(f) {}
+  TestStruct(UndefStruct &us);
 };
+
+struct Y {
+  ~Y() = delete;
+};
+
+struct Z {
+  ~Z() = default;
+};
+
+struct X : Y {};
 
 TEST(test_is_constructible, basic_types) {
   using namespace cpp_utils::traits;
-  static_assert(IsConstructible<int, long>::value, "?");
-
-  static_assert(IsConstructible<int, int &&>::value, "?");
-
-  static_assert(IsConstructible<TestStruct, int, float>::value, "?");
-
-  static_assert(IsConstructible<TestStruct, float, int>::value,
-                "?"); // I don't know why
-
-  static_assert(IsNotConstructible<TestStruct, char *>::value, "?");
+  static_assert(IsConstructible<int, long>::value, "!");
+  static_assert(IsConstructible<int, int &&>::value, "!");
+  static_assert(IsConstructible<TestStruct, int, float>::value, "!");
+  static_assert(IsConstructible<TestStruct, float, int>::value, "!");
+  static_assert(IsConstructible<TestStruct, UndefStruct &>::value, "!");
+  // static_assert(IsConstructible<UndefStruct>::value, "!");
+  static_assert(IsConstructible<Z, Z>::value, "!");
+  static_assert(!IsConstructible<TestStruct, char *>::value, "!");
+  static_assert(IsConstructible<int &&, float>::value, "!");
+  static_assert(!IsConstructible<int &, float>::value, "!");
+  static_assert(!IsConstructible<X, Y>::value, "!");
+  static_assert(!IsConstructible<Y, X>::value, "!");
 }
 
 auto main(int argc, char **argv) -> int {
-
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
