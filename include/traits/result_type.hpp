df213f3eeebcd2fa54232b4ceaf25a428df06a27
L-LYR <lisicen97@gmail.com>
2022-01-02 10:53:39 +0800

    feat: add IsOk & IsErr

--- include/traits/result_type.hpp
+++ include/traits/result_type.hpp
@@ -2,6 +2,7 @@
 #define __CPP_UTILS_TRAITS_RESULT_TYPE__
 
 #include "config/def.hpp"
+#include "traits/bool_utils.hpp"
 
 namespace cpp_utils {
 namespace traits {
@@ -10,6 +11,12 @@ template <config::size_t N> struct ResultType { char _[N]; };
 
 using Ok = ResultType<1>;
 using Err = ResultType<2>;
+
+template <typename T>
+struct IsOk : TypedBoolConstant<sizeof(T) == sizeof(Ok)> {};
+
+template <typename T>
+struct IsErr : TypedBoolConstant<sizeof(T) == sizeof(Err)> {};
 
 } // namespace traits
 } // namespace cpp_utils
